4dd7e9170bfdd43dba10adae98a318829a736469
proxitways <proxitways@gmail.com>
2026-02-20 20:26:00 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener("click", function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute("href"))
-            .scrollIntoView({ behavior: "smooth" });
-    });
+const cards = document.querySelectorAll(".card, .mini-card");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.style.opacity = 1;
+      entry.target.style.transform = "translateY(0)";
+    }
+  });
+},{threshold:0.2});
+
+cards.forEach(card=>{
+  card.style.opacity=0;
+  card.style.transform="translateY(30px)";
+  card.style.transition="all .6s ease";
+  observer.observe(card);
 });
