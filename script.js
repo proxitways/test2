5104d76b36e8bd648dfbed5704afe0efe93fe6cd
proxitways <proxitways@gmail.com>
2026-02-20 20:44:15 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const cards = document.querySelectorAll(".card, .mini-card");
 
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.style.opacity = 1;
+      entry.target.style.transform = "translateY(0)";
+    }
+  });
+},{threshold:0.2});
+
+cards.forEach(card=>{
+  card.style.opacity=0;
+  card.style.transform="translateY(30px)";
+  card.style.transition="all .6s ease";
+  observer.observe(card);
+});
